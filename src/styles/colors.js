c920a587b5f6ee0c059eb69723ca83d09dcab013
David Leuliette <dleuliette@gmail.com>
2018-06-18 13:17:16 +0100

    :new: initial commit

---
+++ src/styles/colors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bg: "#f3f4f7",
+  accent: "#ff4f81",
+  bright: "#ffffff",
+  dark: "#050f2c",
+  gray: "#caccd1"
+};
---
+++ src/utils/config.js
@@ -0,0 +1,23 @@
+const colors = require("../styles/colors");
+
+module.exports = {
+  pathPrefix: "/",
+  appName: "Le Guide du Buteur",
+  siteTitle: "SimpleLanding - a landing page GatsbyJs starter",
+  siteUrl: "https://guidedubuteur.com",
+  siteImage: "preview.jpg",
+  siteLanguage: "fr",
+  siteDescription: "SimpleLanding is a dead simple landing page GatsbyJs starter.",
+  contactEmail: "hello@example.com",
+  ctaLinkUrl: "https://dev.greglobinski.com/gatsby-starter-simple-landing/",
+  ctaLinkLabel: "GatsbyJS starter",
+  // manifest.json
+  manifestName: "SimpleLanding - Gatsby starter",
+  manifestShortName: "SimplLanding",
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.bg,
+  manifestThemeColor: colors.bg,
+  manifestDisplay: "standalone",
+  // analytics
+  analyticsTrackingId: "UA-110088221-3"
+};
